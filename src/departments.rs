461909015d713fe7bae38f4f87667900ccd26a6a
Tr1ckC0 <trick.co@yahoo.com>
2022-07-30 00:35:01 -0500

    implemented basic add / get employees for department

--- src/departments.rs
+++ src/departments.rs
@@ -2,46 +2,74 @@ use std::io;
 use std::error;
 use std::collections::HashMap;
 
-pub fn prompt(){
-    println!("Enter an employee name and departmet.");
-    println!("Example: Add Amir to Marketing");
-    println!("->")
-}
-
 pub fn run() -> Result<(), Box<dyn error::Error>>{
 let mut org_chart: HashMap<String, Vec<String>> = HashMap::new();
     
     loop {
         prompt();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
+        let input = get_input().unwrap();
 
-        if input.trim() == "exit".to_string() {
-            break;
+        let words: Vec<&str> = input.trim().split_whitespace().collect();
+
+        let command = words[0];
+
+        match command {
+            "exit" => break,
+            "add" => add_employee(&words, &mut org_chart)
+                        .unwrap_or_else(handle_invalid_input),
+            "get" => get_department_list(&words, &mut org_chart)
+                        .unwrap_or_else(handle_invalid_input),
+            _ => println!("I don't recognize that command")
         }
-
-        let words: Vec<&str> = input.split(' ').collect();
-
-        if words.len() != 4 {
-            println!("Invalid Input!");
-            println!("Example input: Add Sam to Sales\n");
-            continue;
-        }
-
-        let (name, dep) = (words[1], words[3]);
-
-        let key = String::from(dep);
-
-        org_chart.entry(key)
-        .and_modify(|employee_list| {
-            employee_list.push(String::from(name))
-        })
-        .or_insert(vec![String::from(name)]);
-
-        let department_list = org_chart.get(dep).unwrap();
-
-        println!("{}: {:?}", dep, department_list)
     }
     Ok(())
+}
+
+fn prompt(){
+    println!("Enter an employee name and departmet.");
+    println!("Example: Add Amir to Marketing");
+    println!("->")
+}
+
+fn get_input() -> Result<String, io::Error> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    Ok(input)
+}
+
+fn add_employee(words: &Vec<&str>, org: &mut HashMap<String, Vec<String>>) -> Result<(), &'static str> {
+    if words.len() != 4 {
+        return Err("Should be 4 words.")
+    }
+    
+    let (name, dep) = (words[1], words[3]);
+    let key = String::from(dep);
+
+    org.entry(key)
+    .and_modify(|employee_list| {
+        employee_list.push(String::from(name))
+    })
+    .or_insert(vec![String::from(name)]);
+
+    let department_list = org.get(dep).unwrap();
+
+    println!("{}: {:?}", dep, department_list);
+    Ok(())
+}
+
+fn get_department_list(words: &Vec<&str>, org: &mut HashMap<String, Vec<String>>) -> Result<(), &'static str> {
+    if words.len() != 2 {
+        return Err("Should be 2 words.")
+    }
+    let dep = words[1];
+    match org.get(dep) {
+        Some(list) => println!("Department List: {:?}", list),
+        None => return Err("Could not find that department")
+    }
+    Ok(())
+}
+
+fn handle_invalid_input(message: &str) {
+    println!("Invalid input. {}", message)
 }
